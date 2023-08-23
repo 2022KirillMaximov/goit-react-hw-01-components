d4c80fe76103bee935a6127acb91d8c02de4695a
2022KirillMaximov <102306817+2022KirillMaximov@users.noreply.github.com>
2023-08-23 17:26:54 +0200

    add profile markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
+import user from '../dataFile/user.json'
+
+
+
+import { Profile } from './Profile/Profile';
+
+const {
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+} = user;
+
+
+
+
+
+
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Profile
+          username={username}
+          tag={tag}
+          location={location}
+          avatar={avatar}
+          followers={followers}
+          views={views}
+          likes={likes}
+    />
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+
+export const Profile = ({
+      username,
+  tag,
+  location,
+  avatar = 'https://cdn-icons-png.flaticon.com/512/1077/1077012.png',
+  followers,
+  views,
+  likes,
+}) => {
+  return  (<div class="profile">
+  <div class="description">
+    <img
+      src={avatar}
+      alt="User avatar"
+    />
+          <p >{username}</p>
+          <p >@{tag }</p>
+          <p >{location }</p>
+  </div>
+
+  <ul >
+    <li>
+      <span >Followers</span>
+              <span >{followers }</span>
+    </li>
+    <li>
+      <span >Views</span>
+              <span >{ views}</span>
+    </li>
+    <li>
+      <span >Likes</span>
+              <span >{ likes}</span>
+    </li>
+  </ul>
+</div>)
+}
